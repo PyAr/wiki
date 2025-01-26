3c290a29c3e7ed461bd1b480a4b7881501a38d71
akielbowicz <augusto.kiel@gmail.com>
2025-01-26 19:54:04 -0300

    simplificar el uso del buscador

--- themes/pyar/assets/js/flexsearch.js
+++ themes/pyar/assets/js/flexsearch.js
@@ -1,9 +1,10 @@
-    document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
     var searchIndex = new FlexSearch.Index();  // Initialize FlexSearch
     var index = {};  // This will store the index data globally within this script block
 
     // Fetch the generated JSON file
-    var indexPath = document.location.origin + "/search_index.json"
+    const basePath = "" //document.location.hostname == "localhost" ? "": "/wiki" //Parche para probar en las GHP de mi fork ak.saxa.xyz/wiki
+    var indexPath = document.location.origin + basePath + "/search_index.json"
     fetch(indexPath)
     .then(response => response.json())
     .then(data => {
@@ -15,15 +16,23 @@
         }
     });
 
+    var inputBase = document.getElementById('search_input_base');
     var input = document.getElementById('search_input');
-    var button = document.getElementById('search_button');
 
     // Function to perform search
     function performSearch() {
         var query = input.value;
+  
         var results = searchIndex.search(query);
         var resultsContainer = document.getElementById('search_results');
         resultsContainer.innerHTML = ''; // Clear previous results
+
+        var resultsCount = document.getElementById('search_count');
+        resultsCount.innerHTML = results.length + " resultado(s)";
+        
+        if (input.value === ""){
+            return
+        }
 
         var ul = document.createElement('ul'); // Create a UL element to hold the results
 
@@ -31,39 +40,39 @@
         results.forEach(function(result) {
             var li = document.createElement('li'); // Create a LI element for each result
             var link = document.createElement('a');
-            link.href = index[result].url;
+            link.href = basePath + index[result].url;
             link.textContent = index[result].title;
             li.appendChild(link);
             ul.appendChild(li); // Append the LI to the UL
         });
 
         resultsContainer.appendChild(ul); // Append the UL to the results container
-        document.getElementById('search_overlay').style.display = 'flex'; // Show the overlay
     }
 
-    // Event listener for search button click
-    button.addEventListener('click', performSearch);
+    inputBase.addEventListener('click', ()=>{
+        document.getElementById('search_overlay').style.display = 'flex'; // Show the overlay
+        inputBase.disabled = true;
+        input.focus();
+    })
 
-    // Event listener for pressing enter key in the search input
-    input.addEventListener('keypress', function(event) {
-        if (event.key === "Enter" || event.keyCode === 13) {
-            event.preventDefault();  // Prevent the form from being submitted
-            performSearch();
+    // Event listener for each change in the input field 
+    input.addEventListener('input', performSearch);
+
+// Function to close the search overlay
+    function closeSearch() {
+        document.getElementById('search_overlay').style.display = 'none';
+        document.getElementById('search_input_base').disabled = false;
+        document.getElementById('search_input').value === ""
+    }
+
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'Escape') {
+            var searchOverlay = document.getElementById('search_overlay');
+            if (searchOverlay !== null) {
+                closeSearch();
+            }
         }
     });
 });
 
 
-// Function to close the search overlay
-function closeSearch() {
-    document.getElementById('search_overlay').style.display = 'none';
-}
-
-document.addEventListener('keydown', function(event) {
-    if (event.key === 'Escape') {
-        var searchOverlay = document.getElementById('search_overlay');
-        if (searchOverlay !== null) {
-            closeSearch();
-        }
-    }
-});
